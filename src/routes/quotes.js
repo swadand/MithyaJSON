d6bbc7b7e4031eaaf34b7a39da2ad8d54fdd8243
swadand <dandnaikswapnil@gmaia.com>
2023-10-20 11:54:15 +0530

    Added new Routes, some structure changes

--- src/routes/quotes.js
+++ src/routes/quotes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
-const json = require('../../data/user.json');
+const path = require('path');
+const quotes = require(path.join(__dirname, '../../', 'data/quotes.json'));
 
 router.get('/', (request, response)=> {
     response.setHeader("Content-type", "application/json");
-    response.status(200).send(json);
+    response.status(200).send(quotes);
 });
 
 router.get('*', (request, response)=> {
     response.status(404).send('not found');
 });
 
+//console.log(user);
 module.exports = router;
---
+++ src/routes/users.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const path = require('path');
+const user = require(path.join(__dirname, '../../', 'data/users.json'));
+
+router.get('/', (request, response)=> {
+    response.setHeader("Content-type", "application/json");
+    response.status(200).send(user);
+});
+
+router.get('*', (request, response)=> {
+    response.status(404).send('not found');
+});
+
+//console.log(user);
+module.exports = router;
