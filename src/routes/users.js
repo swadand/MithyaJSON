0b46e84332068b6c27eb9788cf0067673da760d0
swadand <dandnaikswapnil@gmail.com>
2023-10-25 23:02:51 +0530

    Added CRUD routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,9 +8,53 @@ router.get('/', (req, res)=> {
     res.status(200).send(user);
 });
 
+router.post('/add', (req, res)=> {
+    const data = req.body;
+    data.id = 50;
+    res.status(200).send(data);
+});
+
+router.get('/:id', (req, res)=> {
+    const data = user.find(u => u.id.toString() == req.params.id);
+    if(!data){
+        res.status(404).send(JSON.stringify({
+                error: {
+                    "message":"Not Found"
+                }
+        }));
+    }
+    res.status(200).send(data);
+});
+
+router.put('/:id', (req, res)=> {
+    var data = user.find(u => u.id.toString() == req.params.id);
+    if(!data){
+        res.status(404).send(JSON.stringify({
+                error: {
+                    "message":"Not Found"
+                }
+        }));
+    }
+    data = req.body; 
+    res.status(200).send(data);
+});
+
+router.delete('/:id', (req, res)=> {
+    const data = quotes.find(u => u.id.toString() == req.params.id);
+    if(!data){
+        res.status(404).send(JSON.stringify({
+                error: {
+                    "message":"Not Found"
+                }
+        }));
+    }
+    data.deleted = true;
+    res.status(200).send(data);
+});
+
 router.get('*', (req, res)=> {
-    res.status(404);
-    res.sendFile(path.join(__dirname, '../', 'views/404.html'));
+    res.status(404).send();
+    //res.sendFile(path.join(__dirname, '../', 'views/404.html'));
 });
 
 //console.log(user);
